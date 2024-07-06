e209e26097b039c8f51753dbbc2f31915a551b41
igorf08 <igorfmaa@gmail.com>
2024-07-06 00:16:48 -0300

    Estruturação de diretório, configuração de dependências e package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Button from "@mui/material/Button";
+import '@fontsource/roboto/700.css';
+
 function App() {
   return (
     <>
+    <Button variant="contained">Click me!</Button>
     </>
   )
 }
